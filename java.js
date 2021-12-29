08ff9eb7bbdc45feedaa5f7191fe86449fd271ec
Elise <pixbl3@ymail.com>
2021-12-29 16:17:18 -0700

    added pretty colors and dark mode

---
+++ java.js
@@ -0,0 +1,22 @@
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+   }
+   
+   // This function display values
+   function display(value) {
+    document.getElementById("result").value += value;
+   }
+   // This function evaluates the expression and return result
+   function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+   }
+
+   // light dark mode
+
+   function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  } 
